684039a5a6bbf0f238cd3fec8f1c6661b373b340
Conor Macpherson <conmacpherson@hotmail.com>
2022-02-21 21:05:34 -0500

    Update comment model.

--- domains/comment/model.js
+++ domains/comment/model.js
@@ -1,17 +1,18 @@
 class Comment {
-    constructor(content, user_id, url, created_at = null, reply_comment_id = null) {
+    constructor(id, content, user_id, url, parent_comment_id = null, created_at = null, edited_at = null) {
+        this.id = id;
         this.content = content;
         this.user_id = user_id;
         this.url = url;
-        this.original_poster_id = original_poster_id;
-        this.created_at = created_at ?? Date.now()
-        this.reply_comment_id= reply_comment_id
+        this.created_at = created_at ?? Date.now();
+        this.edited_at = edited_at ?? Date.now();
+        this.parent_comment_id = parent_comment_id;
     }
 
     // Getter for Date of the comment, formatted to the user's locale.
     get CommentDate() {
         var language = window.navigator.userLanguage || window.navigator.language;
-        return new Date(this.created_at).toLocaleDateString(language)
+        return new Date(this.created_at).toLocaleDateString(language);
     }
 
     // Getter for the actual content of the comment. Remove any html tags and urls.
